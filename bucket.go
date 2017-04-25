f9ca0d5e8d45e0cf537a211e25eb7f3dc6becf08
Nikolay Glushchenko <nick@nickalie.com>
2017-04-25 11:38:01 +0300

    fixed scan NPE

--- bucket.go
+++ bucket.go
@@ -8,13 +8,13 @@ import (
 	"strings"
 )
 
-//Bucket represents a collection of key/value pairs inside the storage.
+// Bucket represents a collection of key/value pairs inside the storage.
 type Bucket struct {
 	dir  string
 	pool *pool
 }
 
-//GetBucket creates a new bucket if it doesn't already exist and returns a reference to it. Returns an error if the bucket name is invalid.
+// GetBucket creates a new bucket if it doesn't already exist and returns a reference to it. Returns an error if the bucket name is invalid.
 func (b *Bucket) GetBucket(name string) (*Bucket, error) {
 	dir := filepath.Join(b.dir, name)
 	fs := b.pool.Get()
@@ -28,7 +28,7 @@ func (b *Bucket) GetBucket(name string) (*Bucket, error) {
 	return &Bucket{dir: dir, pool: b.pool}, nil
 }
 
-//Set sets the value for a key in the bucket. If the key exist then its previous value will be overwritten.
+// Set sets the value for a key in the bucket. If the key exist then its previous value will be overwritten.
 func (b *Bucket) Set(key string, value []byte) error {
 	fileName := filepath.Join(b.dir, key)
 	fs := b.pool.Get()
@@ -44,7 +44,7 @@ func (b *Bucket) Set(key string, value []byte) error {
 	return afero.WriteFile(fs, fileName, value, 0755)
 }
 
-//Get retrieves the value for a key in the bucket. Returns an error value if the key does not exist.
+// Get retrieves the value for a key in the bucket. Returns an error value if the key does not exist.
 func (b *Bucket) Get(key string) ([]byte, error) {
 	fileName := filepath.Join(b.dir, key)
 	fs := b.pool.Get()
@@ -52,12 +52,17 @@ func (b *Bucket) Get(key string) ([]byte, error) {
 	return afero.ReadFile(fs, fileName)
 }
 
-//Scan executes a function for each key/value pair in a bucket if key has prefix. If the provided function returns false then the iteration is stopped.
+// Scan executes a function for each key/value pair in a bucket if key has prefix. If the provided function returns false then the iteration is stopped.
 func (b *Bucket) Scan(prefix string, fn func(key string, value []byte) bool) {
 	fs := b.pool.Get()
 	defer b.pool.Put(fs)
 
 	afero.Walk(fs, b.dir, func(path string, info os.FileInfo, err error) error {
+
+		if info == nil {
+			return nil
+		}
+
 		if info.IsDir() || filepath.Ext(info.Name()) == ".lock" || !strings.HasPrefix(info.Name(), prefix) {
 			return nil
 		}
@@ -78,7 +83,7 @@ func (b *Bucket) Scan(prefix string, fn func(key string, value []byte) bool) {
 	})
 }
 
-//Remove removes a key from the bucket. If the key does not exist then nothing is done. If no keys provided whole bucket will be removed.
+// Remove removes a key from the bucket. If the key does not exist then nothing is done. If no keys provided whole bucket will be removed.
 func (b *Bucket) Remove(keys ...string) error {
 	fs := b.pool.Get()
 	defer b.pool.Put(fs)
--- fskv.go
+++ fskv.go
@@ -1,3 +1,4 @@
+// Package fskv provides simple file system based key-value storage.
 package fskv
 
 import (
@@ -13,19 +14,19 @@ func (p *pool) Get() afero.Fs {
 	return p.Pool.Get().(afero.Fs)
 }
 
-//DB represents a collection of buckets and key-value pairs that persist on disk.
+// DB represents a collection of buckets and key-value pairs that persist on disk.
 type DB struct {
 	root *Bucket
 }
 
-//Open creates and opens a database at the given path. If the directory does not exist then it will be created automatically.
+// Open creates and opens a database at the given path. If the directory does not exist then it will be created automatically.
 func Open(dir string) (*DB, error) {
 	return OpenWithFactory(func() interface{} {
 		return afero.NewBasePathFs(afero.NewOsFs(), dir)
 	})
 }
 
-//OpenWithFactory creates and opens a database with the given function. Function should return afero.Fs
+// OpenWithFactory creates and opens a database with the given function. Function should return afero.Fs
 func OpenWithFactory(fn func() interface{}) (*DB, error) {
 	p := &pool{
 		&sync.Pool{
@@ -39,27 +40,27 @@ func OpenWithFactory(fn func() interface{}) (*DB, error) {
 	return &DB{&Bucket{dir: "", pool: p}}, nil
 }
 
-//GetBucket creates a new bucket if it doesn't already exist and returns a reference to it. Returns an error if the bucket name is invalid.
+// GetBucket creates a new bucket if it doesn't already exist and returns a reference to it. Returns an error if the bucket name is invalid.
 func (b *DB) GetBucket(name string) (*Bucket, error) {
 	return b.root.GetBucket(name)
 }
 
-//Set sets the value for a key. If the key exist then its previous value will be overwritten.
+// Set sets the value for a key. If the key exist then its previous value will be overwritten.
 func (b *DB) Set(key string, value []byte) error {
 	return b.root.Set(key, value)
 }
 
-//Get retrieves the value for a key. Returns an error value if the key does not exist.
+// Get retrieves the value for a key. Returns an error value if the key does not exist.
 func (b *DB) Get(key string) ([]byte, error) {
 	return b.root.Get(key)
 }
 
-//Scan executes a function for each key/value pair in a bucket if key has prefix. If the provided function returns false then the iteration is stopped.
+// Scan executes a function for each key/value pair in a bucket if key has prefix. If the provided function returns false then the iteration is stopped.
 func (b *DB) Scan(prefix string, f func(key string, value []byte) bool) {
 	b.root.Scan(prefix, f)
 }
 
-//Remove removes a key. If the key does not exist then nothing is done. If no keys provided whole content of the storage wil be removed.
+// Remove removes a key. If the key does not exist then nothing is done. If no keys provided whole content of the storage wil be removed.
 func (b *DB) Remove(keys ...string) error {
 	return b.root.Remove(keys...)
 }
